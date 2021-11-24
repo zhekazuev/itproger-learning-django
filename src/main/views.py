4c90226e684fbb15f82bcac0c6eb5eba5661475f
Eugene Zuev <zhekazuev@gmail.com>
2021-11-24 23:51:31 +0100

    add data-dict to methods

--- src/main/views.py
+++ src/main/views.py
@@ -1,10 +1,37 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, "main/index.html")
+    data = {
+        "title": "main",
+        "values": ["some", "hello", "123"],
+        "object": {
+            "car": "bmw",
+            "age": 2,
+            "hobby": "cars"
+        }
+    }
+    return render(request, "main/index.html", data)
 
 def about(request):
-    return render(request, "main/about.html")
+    data = {
+        "title": "about",
+        "values": ["some", "hello", "123"],
+        "object": {
+            "car": "bmw",
+            "age": 2,
+            "hobby": "cars"
+        }
+    }
+    return render(request, "main/about.html", data)
 
 def contacts(request):
-    return render(request, "main/contacts.html")
+    data = {
+        "title": "contacts",
+        "values": ["some", "hello", "123"],
+        "object": {
+            "car": "bmw",
+            "age": 2,
+            "hobby": "cars"
+        }
+    }
+    return render(request, "main/contacts.html", data)
