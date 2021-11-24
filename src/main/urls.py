a8b61b85114e9e8ac72c373baee3d7f15ae825ff
Eugene Zuev <zhekazuev@gmail.com>
2021-11-24 22:01:36 +0100

    add about and contact urls

--- src/main/urls.py
+++ src/main/urls.py
@@ -17,6 +17,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('about', views.about)
+    path("", views.index, name="home"),
+    path("about", views.about, name="about"),
+    path("contacts", views.contacts, name="contacts")
 ]
