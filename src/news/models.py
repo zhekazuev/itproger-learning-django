1902b544147785eb52d88c6cc46335eb4bd42930
Eugene Zuev <zhekazuev@gmail.com>
2021-11-26 00:49:45 +0100

    add model for Articles and add to admin panel

---
+++ src/news/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Article(models.Model):
+    title = models.CharField("Title", max_length=50)
+    anons = models.CharField("Anons", max_length=250)
+    full_text = models.TextField("Article")
+    date = models.DateTimeField("Publication Date", auto_now=False, auto_now_add=False)
+
+    class Meta:
+        verbose_name = "Article"
+        verbose_name_plural = "Articles"
+
+    def __str__(self) -> str:
+        """String for representing the Article object (in Admin site etc.)."""
+        return f'{self.title}'
