adea1a2b081cf237fb74481e5b376e573b6c96b4
Eugene Zuev <zhekazuev@gmail.com>
2021-11-26 02:26:43 +0100

    add create method

--- src/news/views.py
+++ src/news/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Article
+from .forms import ArticleForm
 
 def news_home(request):
-    news = Article.objects.order_by("date")
+    news = Article.objects.order_by("-date")
     return render(request, "news/news_home.html", {"news": news})
+
+def create(request):
+    error = ""
+    if request.method == "POST":
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("news_home")
+        else:
+            error = "Form is invalid"
+
+    form = ArticleForm()
+    data = {
+        "form": form,
+        "error": error
+    }
+    return render(request, "news/create.html", data)
