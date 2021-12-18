5ee4825ca8d2094687982134bb2f6e19e222ae2e
Eugene Zuev <zhekazuev@gmail.com>
2021-12-18 02:55:41 +0100

    change default static root

--- src/core/settings.py
+++ src/core/settings.py
@@ -152,7 +152,7 @@ STATICFILES_DIRS = (
 )
 
 # For production deployment - STATIC_ROOT="/var/www/example.com/static/"
-STATIC_ROOT = os.getenv("STATIC_ROOT", BASE_DIR / 'staticfiles')
+STATIC_ROOT = os.getenv("STATIC_ROOT", BASE_DIR / 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
