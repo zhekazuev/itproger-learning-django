21373fe8fc2dcd9a166d21cab043d8de64d52e1a
Eugene Zuev <zhekazuev@gmail.com>
2021-12-19 19:16:15 +0100

    add WhiteNoise
    WhiteNoiseMiddleware - for serving static files in one host

--- src/core/settings.py
+++ src/core/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'core.urls'
@@ -157,6 +158,7 @@ STATICFILES_DIRS = (
     BASE_DIR / 'static',
 )
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage' 
 
 MEDIA_URL = '/media/'
 
