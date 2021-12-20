f2cc4284f7e86c169a3a35f617818e0722f2beee
Eugene Zuev <zhekazuev@gmail.com>
2021-12-20 23:15:39 +0100

    add news detail view

--- src/news/views.py
+++ src/news/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import redirect, render
 from .models import Article
 from .forms import ArticleForm
+from django.views.generic import DetailView
 
 def news_home(request):
     news = Article.objects.order_by("-date")
     return render(request, "news/news_home.html", {"news": news})
 
+class NewsDetailView(DetailView):
+    model = Article
+    template_name = "news/details_view.html"
+    context_object_name = 'article'
+
 def create(request):
-    error = ""
+    error = None
     if request.method == "POST":
         form = ArticleForm(request.POST)
         if form.is_valid():
