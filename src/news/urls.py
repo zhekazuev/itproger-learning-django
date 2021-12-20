debcfff141dc45375ee4eb3fb3159155e470d7fe
Eugene Zuev <zhekazuev@gmail.com>
2021-12-20 23:15:28 +0100

    add detailview for news

--- src/news/urls.py
+++ src/news/urls.py
@@ -19,4 +19,5 @@ from . import views
 urlpatterns = [
     path("", views.news_home, name="news_home"),
     path("create", views.create, name="create"),
+    path("<int:pk>", views.NewsDetailView.as_view(), name="news-details"),
 ]
