de3871322bda2f59c3c63d8c3645672427059681
Eugene Zuev <zhekazuev@gmail.com>
2021-12-21 02:04:25 +0100

    add update and delete urls

--- src/news/urls.py
+++ src/news/urls.py
@@ -20,4 +20,7 @@ urlpatterns = [
     path("", views.news_home, name="news_home"),
     path("create", views.create, name="create"),
     path("<int:pk>", views.NewsDetailView.as_view(), name="news-details"),
+    path("<int:pk>/update", views.NewsUpdateView.as_view(), name="news-update"),
+    path("<int:pk>/delete", views.NewsDeleteView.as_view(), name="news-delete"),
+
 ]
